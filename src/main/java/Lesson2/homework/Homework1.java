2842f54e9dac7542674c9853019e6d3cb7e65759
tryazantseva <rtatitata@gmail.com>
2022-11-17 22:03:33 +0300

    Homework_1

--- src/main/java/Lesson2/homework/Homework1.java
+++ src/main/java/Lesson2/homework/Homework1.java
@@ -3,5 +3,47 @@ package Lesson2.homework;
 public class Homework1 {
     public static void main(String[] args) {
 
+        //Задание №1
+
+        double myHeight = 1.68;
+        double myWeight = 58;
+        double result = (myWeight)/(myHeight*myHeight);
+        System.out.println("Индекс массы тела " + result);
+
+        //Задание №2
+
+        int ageChildren = 107;
+        if (ageChildren <= 6) {
+            System.out.println("Пошел в сад");
+        } else if (ageChildren <= 11) {
+            System.out.println("Пошел в младшую школу");
+        } else if (ageChildren <= 17) {
+            System.out.println("Пошел в среднюю школу");
+        } else if (ageChildren > 17) {
+            System.out.println("Пошел в универ");
+
+        //Задание №3
+
+        boolean chicken = false;
+        boolean vegetables = true;
+        boolean sour = true;
+        boolean toast = true;
+        boolean sausage = true;
+        boolean eggs = true;
+
+        boolean resultCaesar = chicken && vegetables && sour && toast;
+        boolean resultOlivie = (chicken || sausage) && vegetables && eggs;
+        boolean resultVegetables  = vegetables;
+
+        if (resultCaesar) {
+            System.out.println("Сделал цезарь");
+        } else if (resultOlivie) {
+            System.out.println("Сделал оливье");
+        } else if (resultVegetables) {
+            System.out.println("Сделал овощной салат");
+        } else {
+            System.out.println("Ничего нет");
+        }
+        }
     }
 }
